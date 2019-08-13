2b8d6ef04199ef4772d58d6a6dbc75bc81eafe9a
akira <chin.kou.akira@gmail.com>
2019-08-13 09:56:45 +0800

    Initial commit

---
+++ src/main/java/ink/akira/demoSpring/controller/HelloController.java
@@ -0,0 +1,14 @@
+package ink.akira.demoSpring.controller;
+
+import ink.akira.demoSpring.pojo.JsonResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/hello")
+    public JsonResult hello() {
+        return JsonResult.success("你好！");
+    }
+}
